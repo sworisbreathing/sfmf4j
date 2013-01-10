f3d0973683e63bab2263167bf520c02defa00d27
Steven Swor <sworisbreathing@gmail.com>
2013-01-10 16:36:16 +1100

    Updating the OSGi test to actually test for event notification.

--- sfmf4j-tests-parent/sfmf4j-tests/src/main/java/sfmf4j/osgi/test/AbstractOSGiTest.java
+++ sfmf4j-tests-parent/sfmf4j-tests/src/main/java/sfmf4j/osgi/test/AbstractOSGiTest.java
@@ -4,14 +4,24 @@
  */
 package sfmf4j.osgi.test;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import static org.ops4j.pax.exam.CoreOptions.*;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import sfmf4j.api.DirectoryListener;
+import sfmf4j.api.FileMonitorService;
 import sfmf4j.api.FileMonitorServiceFactory;
 
 /**
@@ -27,6 +37,9 @@ public abstract class AbstractOSGiTest {
 
     @Inject
     private FileMonitorServiceFactory factoryInstance;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
     protected static String sfmf4jGroupId() {
         return System.getProperty("sfmf4j.groupId");
@@ -53,5 +66,57 @@ public abstract class AbstractOSGiTest {
     public void getFactory() {
         assertNotNull(factoryInstance);
         assertEquals(implementationClassName(), factoryInstance.getClass().getName());
+    }
+
+    @Test
+    public void testFileMonitoring() throws Throwable {
+        assertNotNull(factoryInstance);
+        FileMonitorService fileMonitor = factoryInstance.createFileMonitorService();
+        fileMonitor.initialize();
+        try {
+            final File folder = tempFolder.getRoot();
+
+            final List<File> createdFiles = new LinkedList<File>();
+            final List<File> modifiedFiles = new LinkedList<File>();
+            final List<File> deletedFiles = new LinkedList<File>();
+            final CyclicBarrier barrier = new CyclicBarrier(2);
+            fileMonitor.registerDirectoryListener(folder, new DirectoryListener() {
+
+                public void fileCreated(File created) {
+                    createdFiles.add(created);
+                    try {
+                        barrier.await(10, TimeUnit.SECONDS);
+                    }catch(Exception ex) {
+                        //trap
+                    }
+                }
+
+                public void fileChanged(File changed) {
+                    modifiedFiles.add(changed);
+                    try {
+                        barrier.await(10, TimeUnit.SECONDS);
+                    }catch(Exception ex) {
+                        //trap
+                    }
+                }
+
+                public void fileDeleted(File deleted) {
+                    deletedFiles.add(deleted);
+                    try {
+                        barrier.await(10, TimeUnit.SECONDS);
+                    }catch(Exception ex) {
+                        //trap
+                    }
+                }
+            });
+
+            File newFile = tempFolder.newFile();
+            barrier.await(10, TimeUnit.SECONDS);
+            assertEquals(1, createdFiles.size());
+            assertEquals(newFile.getAbsolutePath(), createdFiles.get(0).getAbsolutePath());
+        }finally {
+            fileMonitor.shutdown();
+        }
+
     }
 }
